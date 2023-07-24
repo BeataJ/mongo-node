0ee42de59e33c47dce485849251c753ac1a87312
beataj <beata@jasinski.com>
2023-07-24 16:53:44 -0700

    posts router

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 const router = express.Router();
 
 const db = require('../data/database')
+
+const ObjectId = mongodb.ObjectId;
 
 router.get('/', function(req, res) {
   res.redirect('/posts');
@@ -16,6 +19,27 @@ router.get('/new-post', async function(req, res) {
   const authors =  await db.getDb().collection('authors').find().toArray();
   // console.log(authors);
   res.render('create-post', {authors: authors});
+});
+
+router.post('/posts', async (req,res) => {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({_id: authorId}) 
+
+  const newPost =  {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email
+    }
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts')
 });
 
 module.exports = router;
