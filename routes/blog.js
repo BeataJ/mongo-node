581cf54a1f984d23ec7be275f083a815e65d5ade
beataj <beata@jasinski.com>
2023-07-24 14:17:36 -0700

    add authors to template

--- routes/blog.js
+++ routes/blog.js
@@ -14,7 +14,8 @@ router.get('/posts', function(req, res) {
 
 router.get('/new-post', async function(req, res) {
   const authors =  await db.getDb().collection('authors').find().toArray();
-  res.render('create-post');
+  // console.log(authors);
+  res.render('create-post', {authors: authors});
 });
 
 module.exports = router;
