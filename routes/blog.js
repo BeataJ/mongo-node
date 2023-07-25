15883993639bdd9787fcd8778373872e24919c4b
beataj <beata@jasinski.com>
2023-07-25 16:32:15 -0700

    update

--- routes/blog.js
+++ routes/blog.js
@@ -74,6 +74,23 @@ router.get('/posts/:id/edit', async (req, res)=> {
     .getDb()
     .collection("posts")
     .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+    if (!post) {
+      return res.status(404).render("404");
+    }
+
+    res.render('update-post', {post: post})
+})
+
+router.post('/posts/:id/edit', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb().collection('posts').updateOne({_id: postId}, {$set: { 
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content
+  }});
+
+  res.redirect('/posts');
 })
 
 module.exports = router;
